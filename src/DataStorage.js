e5c534ba96994ec51d92dd257f2ae58dfad0adfd
formulahunter <gayden.hunter@gmail.com>
2019-03-03 19:06:56 -0500

    # Define class API outline
    * Copy methods from current implementation in **MealPlan**
      * Add unimplemented `edit()` and `delete()` methods
      * Modify sync-reconcile procedure to use `sync()`, `compareHash()`,
      `reconcile()`, and `resolve()` methods
      * Rename ambiguous `getString()` to more descriptive `buffString()`
      * Make `newID()` a getter method

--- src/DataStorage.js
+++ src/DataStorage.js
@@ -8,6 +8,362 @@
 
 class DataStorage {
     constructor() {
-
     }
+
+
+    /** #PUBLIC API
+     *   init()
+     *
+     *   save()
+     *   edit()
+     *   delete()
+     */
+
+
+    /** ##SECTION - Data initialization
+     *
+     *  init()
+     */
+
+    /** Sync data automatically and return parsed JSON data objects
+     *
+     * @returns {object}
+     */
+    init() {}
+
+
+    /** ##SECTION - Incremental data manipulation
+     *
+     */
+
+    /** Save new data instance
+     *
+     * @param {string} type
+     * @param {object} inst
+     *
+     * @returns {Promise}
+     */
+    save(type, inst) {}
+
+    /** Edit existing data instance
+     *
+     * @param {string} type
+     * @param {object} inst
+     *
+     * @returns {Promise}
+     */
+    edit(type, inst) {}
+
+    /** Delete data instance
+     *
+     * @param {string} type
+     * @param {object} inst
+     *
+     * @returns {Promise}
+     */
+    delete(type, inst) {}
+
+
+    /** #PRIVATE API
+     *  Local storage
+     *   _read()
+     *   _write()
+     *
+     *   _sync()
+     *   _compareHash()
+     *   _reconcile()
+     *   _resolve()
+     *
+     *  _hash()
+     *  _buffString()
+     *
+     *  _xhrGet()
+     *  _xhrPost()
+     *
+     *  _encrypt()
+     *  _decrypt()
+     *
+     *  _parse()
+     *  _serialize
+     *  get _dataString()
+     *
+     *   get _lastSync()
+     *   set _lastSync()
+     *
+     *   get _dataString()
+     *
+     *   get _newID()
+     */
+
+
+    /** ##SECTION - Local storage interface
+     *   _read()
+     *   _write()
+     */
+
+    /** Read items from local storage
+     *    Returns `null` if data in local storage is not truthy
+     *    Otherwise returns result of decrypt()
+     *
+     * @param {string} key
+     *
+     * @returns {(string|null)}
+     *
+     * @private
+     */
+    _read(key) {}
+
+    /** Write items to local storage
+     *
+     *
+     * @param {string} key
+     * @param {object} data
+     *
+     * @private
+     */
+    _write(key, data) {}
+
+
+    /** ##SECTION - Sync local storage with remote data file
+     *
+     *   _sync()
+     *   _compareHash()
+     *   _reconcile()
+     *   _resolve()
+     */
+
+    /** Initialize sync-reconcile procedure
+     *    Compare hash digests from both sources
+     *    Initiate reconciliation if digests don't match
+     *
+     *    On success, return a Promise resolving to an object summarizing the sync
+     *    On failure throws an exception
+     *
+     * @param {string} local
+     * @param {string} remote
+     *
+     * @returns {Promise}
+     *
+     * @private
+     */
+    _sync(local, remote) {}
+
+    /** Compare two hash values for equality
+     *
+     * @param {string[]} hashes - Hash digests to compare
+     * @param {string} remote - Remote hash digest
+     * @param {string} local - Local hash digest
+     *
+     * @returns {Promise}
+     *
+     * @private
+     */
+    _compareHash([remote, local]) {}
+
+    /** Reconcile discrepancies
+     *    Aggregate all data activity since last sync
+     *    Send to server's reconciliation script
+     *    Process result by updating local data file
+     *    Pass any conflicts along to `_reconcile()`
+     *
+     * @param {object} result
+     *
+     * @returns {Promise}
+     *
+     * @private
+     */
+    _reconcile(result) {
+        /** Example of documenting return object properties
+         * @param {object} response - An object containing data returned by the server's reconciliation script
+         * @param {string} response.hash - Hash digest of the server's data file computed after running its reconciliation algorithm
+         * @param {string} [response.<type>] - A container object with data instances of the given type, organized by rank
+         * @param {string} [response.<type>.<new|modified|deleted|conflicts>] - Containers for each individual data rank
+         * @param {string} [response.<type>.<new|modified|deleted|conflicts>.<id>] - A data instance of the given type & rank, stored using its ID as a key
+         */
+    }
+
+    /** Resolve server's reconciliation response
+     *
+     * @param {object} response
+     *
+     * @returns {Promise}
+     *
+     * @private
+     */
+    _resolve(response) {}
+
+
+    /** ##SECTION - Compute cryptographic hash digests
+     *
+     *  _hash()
+     *  _getString()
+     */
+
+    /** Initiate hash digest of string values
+     *    String to be hashed may be provided
+     *    If not, data in memory
+     *    Hash algorithm may be specified; defaults to SHA-256
+     *
+     *    Adopted from MDN SubtleCrypto.digest() reference:
+     *    https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#Example
+     *
+     * @param {string} [data=`get _dataString()`] - The string to be hashed
+     * @param {string} [algo=SHA-256] - The hash algorithm to be used (see https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest)
+     *
+     * @returns {Promise} Resolves to the string hash digest
+     *
+     * @private
+     */
+    _hash(data, algo = 'SHA-256') {}
+
+    /** Compile a string from values in the `ArrayBuffer` returned by `crypto.subtle.digest()`
+     *
+     * @param {ArrayBuffer} buff - The `ArrayBuffer` returned by `crypto.subtle.digest()`
+     *
+     * @returns {Promise} Resolves to the string hash digest
+     *
+     * @private
+     */
+    _buffString(buff) {}
+
+
+    /** ##SECTION - Dispatch XHR GET and POST requests
+     *
+     *  _xhrGet()
+     *  _xhrPost()
+     */
+
+    /** Dispatch XMLHttpRequests with the GET method
+     *
+     * @param {string} url - The URL of the request target file
+     * @param {string[]} [headers=[]] - An array of request headers to be set on the XHR object before it is sent
+     *
+     * @returns {Promise} Resolves to the value of the server response
+     *
+     * @private
+     */
+    _xhrGet(url, headers = []) {}
+
+    /** Dispatch XMLHttpRequests with the POST method
+     *
+     * @param {string} data - The body of the request to be sent
+     * @param {string} url - The URL of the request target file
+     * @param {string[]} [headers=[]] - An array of key-value string pairs as request headers to be set on the XHR object before it is sent
+     *
+     * @returns {Promise} Resolves to the value of the server response
+     *
+     * @private
+     */
+    _xhrPost(data, url, headers = []) {}
+
+
+    /** ##SECTION - Encryption
+     *
+     *  _encrypt()
+     *  _decrypt()
+     */
+
+    /** Encrypt a string with a given password
+     *    Presently just an alias for `sjcl.encrypt()`
+     *
+     * @param {string} data - Data string to be encrypted
+     * @param {string} [password='password'] - The password to be used as the cryptographic key
+     *
+     * @returns {string} The encrypted text
+     *
+     * @private
+     */
+    _encrypt(data, password = 'password') {}
+
+    /** Decrypt a cipher with a given password
+     *    Presently just an alias for `sjcl.decrypt()`
+     *
+     * @param {string} cipher - Encrypted cipher to be decrypted
+     * @param {string} [password='password'] - The password to be used as the cryptographic key
+     *
+     * @returns {string} The decrypted text
+     *
+     * @private
+     */
+    _decrypt(cipher, password = 'password') {}
+
+
+    /** ##SECTION - JSON conversions
+     *
+     *  _parse()
+     *  _serialize
+     *  get _dataString()
+     */
+
+    /** Parse a JSON string into an Javascript Object
+     *    Presently just an alias for `JSON.parse()`
+     *
+     * @param {string} jstr - The string to be parsed
+     * @param {function} [reviver] - A function that parses given JSON text in a specific way
+     *
+     * @returns {*}
+     *
+     * @private
+     */
+    _parse(jstr, reviver) {}
+
+    /** Serialize a JavaScrip Object into a string
+     *    Presently just an alias for `JSON.parse()`
+     *
+     * @param {string} val - The string to be parsed
+     * @param {function} [replacer] - A function that replaces property values with alternate data for parsing
+     *
+     * @returns {string}
+     *
+     * @private
+     */
+    _serialize(val, replacer) {}
+
+    /** Serializing all data instances in a consistent format
+     *
+     * @returns {string} JSON string of all data instances stored in `.types` object
+     *
+     * @private
+     * @readonly
+     */
+    get _dataString() {}
+
+
+    /** ##SECTION - Maintain last-sync parameter
+     *
+     *  get _lastSync()
+     *  set _lastSync()
+     */
+
+    /** Get timestamp of the most recent successful sync
+     *
+     * @returns {number} The timestamp of the most recent successful sync
+     *
+     * @private
+     */
+    get _lastSync() {}
+    /** Set timestamp of the most recent successful sync
+     *
+     * @param {number} sync
+     *
+     * @returns {undefined}
+     *
+     * @private
+     */
+    set _lastSync(sync) {}
+
+
+    /** ##SECTION - ID assignment
+     *
+     *  get _newID()
+     */
+
+    /** Ensure that no two data instances are assigned the same id during batch saves
+     *
+     * @returns {number} The smallest integer greater than or equal to the current timestamp which has not already been assigned as an ID to another data instance
+     *
+     * @private
+     * @readonly
+     */
+    get _newID() {}
 }
