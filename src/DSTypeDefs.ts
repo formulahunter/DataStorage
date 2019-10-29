c6ea5290051f707ddab2636e5c0e59ab9945c6a1
formulahunter <gayden.hunter@gmail.com>
2019-10-29 11:17:32 -0400

    # Complete the `DSDataRecord` implementation
    > *Remaining methods (`copy()`, `get id`, `toString()`) are migrated.
    > Substantial revisions are made to the static `fromJSON()` **implementation**
    > and documentation, although its **interface (signature) remains unaffected**.
    > Note that the **TypeScript** compiler was used for the first time; it was
    > installed via **npm**, which created `package.json` & `src/index.js` and
    > modified `README.md` & `tsconfig.json`.*
    
    
    ## Summary
    
    * Define `DSDataRecord#copy()`, making use of **TypeScript**'s `this` type for
      the return type
      * Optional boolean argument indicates whether or not the private `_created`
        and `_modified` properties should be copied
      * Calling an object's constructor using `this.constructor()` causes
        **TypeScript** to throw an error as it is not clear at compile time that the
        call signature will match the function signature of the constructor
        * depending on what object the `this` keyword is assigned to at runtime, the
          constructor may not be the same as the one of the object/prototype for
          which the call was originally defined
    * Update `DSDataRecord#fromJSON()` documentation to mark one paragraph private
    * Refine the `DSDataRecord#fromJSON()` implementation:
      1. Use a union type rather than overloads per [the TypeScript Handbook][use union types]
      2. Explicitly verify the `_created` and `_modified` properties of the
         argument, or the object obtained by parsing the argument (if a `string`)
    * Define `DSDataRecord#toString()`
    
    
    [use union types]: https://www.typescriptlang.org/docs/handbook/declaration-files/do-s-and-don-ts.html#use-union-types
        "Do's and Don'ts - TypeScript | Microsoft | October 29, 2019"

--- src/DSTypeDefs.ts
+++ src/DSTypeDefs.ts
@@ -70,33 +70,46 @@ class DSDataRecord {
 
     /** Get a `DSDataRecord` from a **JSON** object or string
      *
-     * @remarks
+     * @privateRemarks
      * This method explicitly checks the presence and types of the
      * `DSDataRecord` properties `_created`, which must be defined *and* a
      * `number`; and `_modified`, which must be `undefined` *or* a `number`.
      *
      * @param json - a JSON `object` or `string`
+     *
+     * @throws TypeError
+     * Thrown if the provided object or string is not a valid `DSDataJSONRecord`
      */
-    static fromJSON(json: DSDataJSONRecord): DSDataRecord;
-    static fromJSON(json: string): DSDataRecord;
-    static fromJSON(json: any): any {
+    static fromJSON(json: DSDataJSONRecord|string): DSDataRecord {
         let inst: DSDataRecord = new this();
 
         let jobj: DSDataJSONRecord;
         if(typeof json === 'string') {
-            let obj: any = DataStorage.parse(json);
-            if(obj._created === undefined || typeof obj._created !== 'number')
-                throw new TypeError(`Faulty JSON string ${json} does not define a _created property`);
-            if(obj._modified !== undefined && typeof obj._modified !== 'number')
-                throw new TypeError(`Faulty JSON string ${json} specifies an invalid _modified property`);
+            let obj: {_created?: any, _modified?: any} = DataStorage.parse(json);
+            if(obj._created === undefined || typeof obj._created !== 'number') {
+                let er: TypeError = new TypeError(`A required private property is missing: ${json}`);
+                er.name = 'InvalidJsonString';
+                throw er;
+            }
+            if(obj._modified !== undefined && typeof obj._modified !== 'number') {
+                let er: TypeError = new TypeError(`A required private property is invalid: ${json}`);
+                er.name = 'InvalidJsonString';
+                throw er;
+            }
 
-            jobj = obj;
+            jobj = <DSDataJSONRecord>obj;
         }
         else {
-            if(json._created === undefined || typeof json._created !== 'number')
-                throw new TypeError(`Faulty JSON object ${json} does not define a _created property`);
-            if(json._modified !== undefined && typeof json._modified !== 'number')
-                throw new TypeError(`Faulty JSON object ${json} specifies an invalid _modified property`);
+            if(json._created === undefined || typeof json._created !== 'number') {
+                let er: TypeError = new TypeError(`Faulty JSON object ${json} does not define a _created property`);
+                er.name = 'InvalidJsonObject';
+                throw er;
+            }
+            if(json._modified !== undefined && typeof json._modified !== 'number') {
+                let er: TypeError = new TypeError(`${json} specifies an invalid _modified property`);
+                er.name = 'InvalidJsonObject';
+                throw er;
+            }
 
             jobj = json;
         }
@@ -108,15 +121,70 @@ class DSDataRecord {
         return inst;
     }
 
-    /** Get an object literal representing this data instance, with the intent
+
+    /** Public getter method of the instance ID
+     *
+     * @privateRemarks
+     * ID is defined as the private `created` property
+     *
+     * @returns the instance ID
+     */
+    get id(): number {
+        return this.created;
+    }
+
+
+    /** Return a new instance whose properties values are identical to the
+     * current instance
+     *
+     * @privateRemarks
+     * This method excludes the `_created` and `_modified` properties unless the
+     * optional `id` argument is `true`. Subclasses must override this
+     * method and instantiate their return instance using `super.copy()`
+     *
+     * @param id - if `true`, the instance's ID property is also copied;
+     *        defaults to `false`
+     *
+     * @returns a copy of this instance
+     */
+    copy(id: boolean = false): this {
+        let copy: this;
+        if(id === true) {
+            // @ts-ignore
+            copy = new this.constructor(this.id);
+        }
+        else {
+            // @ts-ignore
+            copy = new this.constructor();
+        }
+
+        return copy;
+    }
+
+
+    /** Get an object literal representing this data instance with the intent
      *  to serialize to a JSON string. Subclasses must implement this method
      *  and initiate their return value
      *  using `super.toJSON()`
+     *
+     *  @returns a JSON object literal representing the instance
      */
     toJSON(): object {
         return {
             _created: this.created,
             _modified: this.modified === -1 ? undefined : this.modified
         };
+    }
+
+
+    /** Get a human-readable string representation of the data instance, e.g.
+     *  for console output. Subclasses must override this method
+     *
+     * @returns the class name and instance ID separated by a hash tag
+     *
+     * @override
+     */
+    toString(): string {
+        return `${this.constructor.name}#${this.id}`;
     }
 }
